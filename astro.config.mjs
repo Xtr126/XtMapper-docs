d096e093866f002785fbd2ee348f30015c3bd643
Xtr126 <crawleymsh@gmail.com>
2023-09-26 01:06:50 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://xtr126.github.io',
+	base: '/XtMapper-docs',
 	integrations: [
 		starlight({
 			title: 'XtMapper',
