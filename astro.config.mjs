5c3c7a5d08a204229a68121afb91f53a571da0cc
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-12-12 09:53:25 +0530

    fix: sharp not found error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://xtr126.github.io',
 	base: '/XtMapper-docs',
+	image: {
+   		service: passthroughImageService(),
+ 	},
 	integrations: [
 		starlight({
 			title: 'XtMapper',
